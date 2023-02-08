32a51ed6ff73f002fc774c6529ce531028782bbf
Haris Sumair <sumair.haris@gmail.com>
2023-02-09 00:27:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+import gdown
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Hello Verceller"
+
+
+@app.route('/run')
+def run_colab():
+    gdown.download('https://colab.research.google.com/drive/1TlhVz7CA8L0AC1s3tmZqVOxtiKmyVk6z','GPTTrader.ipynb', quiet=False)
+    return jsonify(message='colab notebook ran successfully')

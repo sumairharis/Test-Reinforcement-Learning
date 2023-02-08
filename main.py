2a3fabb48a3014e6c98630e39c63ab91380af0b8
Haris Sumair <sumair.haris@gmail.com>
2023-02-09 01:00:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/run')
 def run_colab():
-    #gdown.download('https://colab.research.google.com/drive/1TlhVz7CA8L0AC1s3tmZqVOxtiKmyVk6z','GPTTrader.ipynb', quiet=False)
+    gdown.download('https://colab.research.google.com/drive/1TlhVz7CA8L0AC1s3tmZqVOxtiKmyVk6z','GPTTrader.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
 
 
 @app.route("/")
 def home():
-    return "Hello Verceller wes latest"
+    return "Hello Verceller wes latest with gdown"
 

ce45150eafd86d2f8248a8fe892ae33abcc2d116
Haris Sumair <sumair.haris@gmail.com>
2023-02-09 00:32:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,5 @@ def run_colab():
 
 @app.route("/")
 def home():
-    return "Hello Verceller"
+    return "Hello Verceller wes"
 
-
-@app.route('/run')
-def run_colab():
-    gdown.download('https://colab.research.google.com/drive/1TlhVz7CA8L0AC1s3tmZqVOxtiKmyVk6z','GPTTrader.ipynb', quiet=False)
-    return jsonify(message='colab notebook ran successfully')
